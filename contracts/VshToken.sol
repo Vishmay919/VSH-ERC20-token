1fb5c7510e34dc8ae5b563fe2071ce03ba09ce90
Vishmay <vishmaychauhan11@gmail..com>
2022-01-13 23:29:47 +0530

    added approval and delegate transfer functionality

--- contracts/VshToken.sol
+++ contracts/VshToken.sol
@@ -13,15 +13,24 @@ contract VshToken {
       uint256 _value
   );
 
+  event Approve(
+      address indexed _owner,
+      address indexed _spender,
+      uint256 _value
+  );
+
   uint256 public totalSupply;
   mapping(address => uint256) public balanceOf;
+  //Account A approves acount B,C,D.. to spend value
+  mapping(address => mapping(address => uint256)) public allowance;
+
 
   constructor(uint256 _initialSupply) {
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
   }
 
-  function transfer(address _to, uint256 _value) public returns(bool){
+    function transfer(address _to, uint256 _value) public returns(bool){
       require(balanceOf[msg.sender] >= _value);
 
       balanceOf[msg.sender] -= _value;
@@ -31,7 +40,27 @@ contract VshToken {
 
       return true;
       
-  }
+    }
+
+    function approve(address _spender, uint256 _value) public returns(bool){
+        allowance[msg.sender][_spender] = _value;
+        emit Approve(msg.sender,_spender,_value);
+        return true;
+    }
+
+    function transferFrom(address _from,address _to,uint256 _value) public returns(bool){
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from,_to, _value);
+        return true;
+    }
+
 
 
 }
--- test/VshToken.js
+++ test/VshToken.js
@@ -55,6 +55,67 @@ contract("VshToken",function(accounts){
         }).then((bal)=>{
             assert.equal(bal,750000,"deducts the amount from sender")
         })
+    });
+
+    it("approves token for delegated transfer",()=>{
+        return VshToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1],100);
+        }).then((success)=>{
+            assert.equal(success,true,"it returns true");
+            return tokenInstance.approve(accounts[1],100,{from: accounts[0]});
+        }).then((receipt)=>{
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Approve', 'should be the "Approval" event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then((allowance)=>{
+            assert.equal(allowance.toNumber(),100,"stores the allowance for delegated transfer");
+        })
+    });
+
+    it("handles delegated transfer",()=>{
+        return VshToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4]
+            //transferring some tokens to from account
+            return tokenInstance.transfer(fromAccount,100,{from: accounts[0]});
+    }).then((receipt)=>{
+        //approve spendingAccount to spend 10 tokens from FromAccount
+        return tokenInstance.approve(spendingAccount,10,{from: fromAccount})
+    }).then(()=>{
+        //transfer token more than that in spender account
+        return tokenInstance.transferFrom(fromAccount,toAccount,999,{from : spendingAccount});
+    }).then(assert.fail).catch((error)=>{
+        assert(error.message.indexOf('revert') >=0,"cannot transfer more than balance");
+        //try spending more tokens than the approved amount
+        return tokenInstance.transferFrom(fromAccount, toAccount,20,{from: spendingAccount})
+    }).then(assert.fail).catch((error)=>{
+        assert(error.message.indexOf('revert') >=0,"cannot transfer more than approved balance");
+        return tokenInstance.transferFrom.call(fromAccount,toAccount,10,{from: spendingAccount})
+    }).then((success)=>{
+        assert.equal(success,true,"it returns true")
+        return tokenInstance.transferFrom(fromAccount,toAccount,10,{from: spendingAccount});
+    }).then((receipt)=>{
+        assert.equal(receipt.logs.length, 1, 'triggers one event');
+        assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+        assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
+        assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
+        assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+        return tokenInstance.balanceOf(fromAccount)
+    }).then((bal)=>{
+        assert.equal(bal.toNumber(),90,"deducts balance from sending account")
+        return tokenInstance.balanceOf(toAccount)
+    }).then((bal)=>{
+        assert.equal(bal.toNumber(),10,"adds balance to _to account")
+        return tokenInstance.allowance(fromAccount,spendingAccount)
+    }).then((bal)=>{
+        assert.equal(bal.toNumber(),0,"deducts amount from allowance")
     })
+})
     
 })
